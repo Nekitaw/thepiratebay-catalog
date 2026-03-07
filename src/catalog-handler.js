c4070b61592163be6b71fe2ecb2508d4edfd9237
Naoki <max_fabian@hotmail.com.ar>
2026-03-07 03:39:31 -0300

    Updated readme

--- src/catalog-handler.js
+++ src/catalog-handler.js
@@ -98,7 +98,8 @@ const fetchTorrents = ({categoryId, args}) =>
 					search(
 						args.extra.search,
 						cond([
-							[propEq('id', 'Movies'), () => 201],
+							[propEq('id', 'Videos'), () => 200],
+							[propEq('id', 'Movies'), () => 200],
 							[propEq('id', 'Porn'), () => 500],
 							[propEq('id', 'Porn recent'), () => 600],
 							[propEq('id', 'TV shows'), () => 205],
